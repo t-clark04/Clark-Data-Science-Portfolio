d25f1f239471237fb45b6cd2fd36731c603533bd
t-clark04 <t_clark04@yahoo.com>
2025-04-13 12:58:59 -0400

    Added comments to user-inputted data section

--- MLStreamlitApp/app.py
+++ MLStreamlitApp/app.py
@@ -312,33 +312,40 @@ if path == "Upload my own dataset!":
                 """)
     st.write("On your mark, get set, go!")
 
+    # Prompt the user to upload a file.
     uploaded_file = st.file_uploader("Upload a .csv file containing the tidy dataset of interest with at least one binary predictor (and no dates!):", type = "csv")
-    if uploaded_file:
-        input_data = pd.read_csv(uploaded_file)
+    if uploaded_file: # If they do upload a csv file...
+        input_data = pd.read_csv(uploaded_file) # Read it in.
         st.write("Here are the first few rows of your dataset (missing values will be dropped):")
-        st.dataframe(input_data.head())
-        input_data = input_data.dropna()
+        st.dataframe(input_data.head()) # Display the first 5 rows.
+        input_data = input_data.dropna() # Drop all missing values (two of the ML models can't deal with them).
+        # Create a list with all possible binary variables in the inputted dataset.
         pos_binary = []
         for var in input_data.columns:
             if input_data[var].nunique() == 2:
                 pos_binary.append(var)
-        if len(pos_binary) == 1:
+        if len(pos_binary) == 1: # If there's only one, that's the target variable.
             target = pos_binary[0]
             st.write(f"The only binary variable in your dataset is {target}. This will be used as your target variable.")
-        elif len(pos_binary) > 1:
+        elif len(pos_binary) > 1: # If there are multiple, prompt the user to select one as target.
             target = st.selectbox("Please select which binary variable you would like to use as your target:", pos_binary)
-        else:
+        else: # If there are no binary variables in the dataset, ask the user to upload a different one.
             st.write("This dataset contains no binary variables. Please select a new dataset.")
             st.stop()
-        input_data_numeric = input_data.copy()
+        input_data_numeric = input_data.copy() # Create a copy of the dataset before converting all categorical variables to numeric.
+        # If the values of the target variables aren't already 0 and 1, changes them to 0s and 1s,
+        # then store the map so we can recover what the 0s and 1s mean.
         if set(input_data[target].unique()) != {0,1}:
             target_map = {input_data[target].unique()[0]:0,
                    input_data[target].unique()[1]:1}
             input_data_numeric[target] = input_data[target].map(target_map)
+        # Have the user select their feature variables from those given in the inputted dataset.
         vars = list(input_data.columns)
         vars.remove(target)
         features = st.multiselect("Select variables from the dataset to build your model:", vars)
-        if features:
+        if features: # Once they select at least one variable...
+            # Convert all of the "bool" columns to 0s and 1s, then store all possible categorical
+            # variables in a list called cats.
             cats = []
             for var in features:
                 if input_data[var].dtype == "bool":
@@ -347,6 +354,9 @@ if path == "Upload my own dataset!":
                     input_data_numeric[var] = input_data[var].map(map)
                 elif input_data[var].dtype == "object":
                     cats.append(var)
+            # Go through each of the variables in the cats list and convert them to numeric values in the 
+            # data frame. Store the value map for each variable in a large dictionary called cat_dict, so
+            # that we can recover what each numeric value represents later on.
             cat_dict = {}
             for cat in cats:
                 map = {}
@@ -354,11 +364,18 @@ if path == "Upload my own dataset!":
                     map[input_data[cat].unique()[i]] = i
                 input_data_numeric[cat] = input_data[cat].map(map)
                 cat_dict[cat] = map
+            # Begin the process of gathering user data for their inputted dataset.
             user_data = []
-            for var in features:
+            for var in features: # Go through each of the variables selected as features.
+                # If it's a boolean or a categorical variable, display the original values of the variable
+                # as options in a selectbox, but store the numeric counterpart of the value in our user_data
+                # list, according to the map defined above.
                 if input_data[var].dtype == "bool" or input_data[var].dtype == "object":
                     val = st.selectbox(f"Please choose a value for {var}:", list(input_data[var].unique()))
                     user_data.append(cat_dict[var][val])
+                # If it's an integer variable with 10 or fewer values, use a selectbox widget to gather
+                # the user input and store it accordingly. If it's an integer variable with more than 10
+                # unique values, use a slider instead.
                 elif (input_data[var] % 1 == 0).all():
                     if len(input_data[var].unique()) <= 10:
                         sorted_list = sorted([int(val) for val in input_data[var].unique()])
@@ -369,30 +386,34 @@ if path == "Upload my own dataset!":
                         val = st.slider(f"Please choose a value for {var}:", min_value = int(input_data[var].min()),
                                         max_value = int(input_data[var].max()), step = 1)
                         user_data.append(val)
+                # If it's a decimal variable, use the general number_input widget to gather the user's data
+                # and store it in the user_data list.
                 elif input_data[var].dtype == "float64":
                     val = st.number_input(f"Please enter a value for {var}:")
                     user_data.append(val)
-            user_data = [user_data]
+            user_data = [user_data] # Reformatting the user_data list according to the requirements of later functions.
             model_choice = st.radio("Choose a classification model:", ['Logistic Regression', 'Decision Tree', 'K-Nearest Neighbors'])
 
+            # Logistic Regression path
             if model_choice == "Logistic Regression":
+                # Only "hyperparameter" for logistic regression in this model is scaled/unscaled data.
                 scale_question = st.radio("Would you like to scale the data? (Scaling adjusts for unit bias.)", ['Yes', 'No'])
-    
+                # Where model execution starts.
                 if st.button("Run!"):
-                    X,y = data_prep(input_data_numeric, features, target)
-                    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2,
-                                                                random_state = 99)
-                    if scale_question == "Yes":
+                    X,y = data_prep(input_data_numeric, features, target) # Subset data into X,y.
+                    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 99) # Split into training and testing sets.
+                    if scale_question == "Yes": # Re-scale the data if the user desires.
                         X_train, X_test, user_data = scale_data(X_train, X_test, user_data)
                     model = LogisticRegression()
-                    model.fit(X_train, y_train)
-                    model_prob2(model, user_data)
+                    model.fit(X_train, y_train) # Define and fit the logistic regression model.
+                    model_prob2(model, user_data) # Use the model to make and display predictions on the user's data.
                     y_pred = model.predict(X_test)
-                    model_metrics(y_test, y_pred)
-                    if set(input_data[target].unique()) != {0,1}:
+                    model_metrics(y_test, y_pred) # Calcualte and display the model metrics.
+                    if set(input_data[target].unique()) != {0,1}: # Specify what 0 and 1 represent in the confusion matrix and classification report.
                         st.write(f"(Here, 0 represents {input_data[target].unique()[0]} and 1 represents {input_data[target].unique()[1]}.)")
-                    display_visuals(y_test, y_pred, X_test)
+                    display_visuals(y_test, y_pred, X_test) # Display the confusion matrix, classification report, and ROC curve.
 
+            # Decision Tree path
             if model_choice == 'Decision Tree':
                 hyper_choice = st.radio("Would you like to choose your own model hyperparameters, or have the model optimize them for you?",
                                 ["I'll tune them myself.", "Tune them for me!"])
